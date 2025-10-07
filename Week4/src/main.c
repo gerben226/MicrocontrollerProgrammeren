128c53ee7ba9832f64f6d85b2b96c2aa0b4af5d1
gerben226 <s1202232@student.windesheim.nl>
2025-10-07 23:17:43 +0200

    add button isr

--- Week4/src/main.c
+++ Week4/src/main.c
@@ -6,18 +6,36 @@ WDTCTL = WDTPW | WDTHOLD;  	 //stop WDT
 PM5CTL0 &= ~LOCKLPM5;		//disable the GPIO power-on default HI-mode
 
 pinSetDir(PORT1, BIT0, 1);
-pinConfigInput(PORT1, BIT1, 1, 1, 0, 0);
+pinConfigInput(PORT1, BIT1, 1, 1, 1, 1);
 pinSet(PORT1, BIT0, false);
-
+__bis_SR_register(GIE);
 while(1)
 {
+// if(!(pinGet(PORT1, BIT1))){
+//     pinSet(PORT1, BIT0, 1);
+// }
+// else{
+//     pinSet(PORT1, BIT0, 0);
+// }
 
-if(!(pinGet(PORT1, BIT1))){
-    pinSet(PORT1, BIT0, 1);
-}
-else{
-    pinSet(PORT1, BIT0, 0);
 }
 
 }
+
+#pragma vector = PORT1_VECTOR
+__interrupt void pushbutton_ISR(void) {
+    switch(__even_in_range(P1IV, 0x10)){
+        case 0x00: break; //none
+        case 0x02: break; //pin 0
+        case 0x04:        //pin 1
+         P1OUT ^= BIT0;
+        break;
+        case 0x06: break; //pin 2
+        case 0x08: break; //pin 3
+        case 0x0A: break; //pin 4
+        case 0x0C: break; //pin 5
+        case 0x0E: break; //pin 6
+        case 0x10: break; //pin 7
+        default: _never_executed();
+    }
 }
