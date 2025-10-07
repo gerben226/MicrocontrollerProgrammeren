e2d22b462c98eb1ba50d003948b0a90cfd844510
gerben226 <s1202232@student.windesheim.nl>
2025-10-07 08:51:52 +0200

    add function to toggle pin

--- Week4/src/main.c
+++ Week4/src/main.c
@@ -6,10 +6,7 @@ int main(void)
 WDTCTL = WDTPW | WDTHOLD;  	 //stop WDT
 PM5CTL0 &= ~LOCKLPM5;		//disable the GPIO power-on default HI-mode
 
-P4DIR |= BIT6;
 pinSet(PORT4, BIT6, true);
-
-
 
 while(1)
 {
